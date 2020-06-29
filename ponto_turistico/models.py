6e06890a33a9aa9697aa22a062bb2c2af424f831
WillamesCampos <51215232+WillamesCampos@users.noreply.github.com>
2020-06-29 01:17:08 -0300

    (Aula41)Adicionando campo OneToOne em PontoTuristico

--- ponto_turistico/models.py
+++ ponto_turistico/models.py
@@ -4,6 +4,11 @@ from comentarios.models import Comentario
 from avaliacoes.models import Avaliacao
 from endereco.models import Endereco
 
+
+class DocIdentificacao(models.Model):
+    descricao = models.TextField()
+
+
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
@@ -18,6 +23,9 @@ class PontoTuristico(models.Model):
         Endereco, on_delete=models.CASCADE, null=True, blank=True
     )
     foto = models.ImageField(upload_to='pontos_turisticos', blank=True, null=True)
+    doc_identificacao = models.OneToOneField(
+        DocIdentificacao, on_delete=models.CASCADE, null=True, blank=True
+    )
 
     def __str__(self):
         return self.nome
