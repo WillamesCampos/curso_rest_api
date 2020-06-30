1806c9ba245da58685fece1c53e9d5cec822f35f
WillamesCampos <51215232+WillamesCampos@users.noreply.github.com>
2020-06-30 02:36:31 -0300

    (Aula 44) Action personalizada no admin

--- comentarios/admin.py
+++ comentarios/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Comentario
+from .actions import reprova_comentarios, aprova_comentarios
 
-admin.site.register(Comentario)
+
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ['usuario', 'data', 'aprovado']
+    actions = [reprova_comentarios, aprova_comentarios]
+
+
+admin.site.register(Comentario, ComentarioAdmin)
 
 # Register your models here.
